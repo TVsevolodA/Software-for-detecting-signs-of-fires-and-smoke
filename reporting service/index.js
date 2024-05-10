c35993d3cca50c8040db942198d44d9a9023a73a
Vsevolod <tri-vsal@yandex.ru>
2024-05-10 10:25:02 +0400

    Добавил страницы для обработки ошибок на сервере отчетов.

--- reporting service/index.js
+++ reporting service/index.js
@@ -2,12 +2,12 @@
 const reportManager = require('./report_manager');
 
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
 const urlencodedParser = express.urlencoded({extended: false});
 const session = require('express-session');
-
 
 app.use(bodyParser.json())
 app.use(
@@ -26,6 +26,8 @@ app.listen(port, () => {
 console.log(`App running on port ${port}.`)
 })
 
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
 app.get('/', (request, response) => {
   console.log('Запрос получен!');
   response.json({ info: 'Сервис обработки отчетов' });
@@ -37,3 +39,11 @@ app.get('/getReports/:id/:username/:role',    (request, response) => reportManag
 app.get('/getReports',    (request, response) => reportManager.getReports(request, response));
 app.get('/updateReport/:id', (request, response) => reportManager.updateReport(request, response));
 app.post('/updateReport/:id', urlencodedParser, (request, response) => reportManager.updateReport(request, response));
+
+app.use(function(req,res){
+  res.status(404).render('error404.hbs');
+});
+
+app.use(function(req,res){
+  res.status(500).render('error500.hbs');
+});
